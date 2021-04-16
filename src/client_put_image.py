1a3e4ba7d9784a71fa821e8228cea4e5956fc7fa
SuryaAssistant <09.fandisurya@gmail.com>
2021-04-16 07:00:36 +0700

    change ip

---
+++ src/client_put_image.py
@@ -0,0 +1,59 @@
+# ----------------------------------------------------------------------------------------------------
+# Example program which support file transfer
+# from client and server through CoAP using aiocoap
+# Created by Fandi Adinata @2021 <https://githun.com/SuryaAssistant/coapfiletransfer>
+#
+# Thanks to aiocoap  
+# Copyright (c) 2012-2014 Maciej Wasilak <http://sixpinetrees.blogspot.com/>,
+#               2013-2014 Christian Amsüss <c.amsuess@energyharvesting.at>
+#
+# ----------------------------------------------------------------------------------------------------
+
+import logging
+import asyncio
+
+from aiocoap import *
+
+import base64
+import os
+import datetime
+
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    """The payload is bigger than 1kB, and thus sent as several blocks."""
+
+    # server ip address as destination
+    #ip_server = "127.0.0.1"
+    ip_server = "192.168.43.115"
+
+    # file location
+    transferred_file = "./sample_image/sample_image_1.jpg"
+
+    # get file information
+    file_name, file_extension = os.path.splitext(transferred_file)
+
+    # encode to base64 representation
+    with open("{}".format(transferred_file), "rb") as source_file:
+        str_encode = base64.b64encode(source_file.read())
+
+    # set as "fully" string content
+    str_to_send = str(str_encode.decode('ascii', 'ignore'))
+
+    context = await Context.create_client_context()
+
+    await asyncio.sleep(2)
+
+    # combine with file information and encode as ascii
+    pre_payload = str(file_name) + (",") + str(file_extension) + (",") + str_to_send
+    payload  = pre_payload.encode('ascii')
+
+    # send PUT to server
+    request = Message(code=PUT, payload=payload, uri="coap://{}/image".format(ip_server))
+
+    response = await context.request(request).response
+
+    print('Result: %s\n%r'%(response.code, response.payload))
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
